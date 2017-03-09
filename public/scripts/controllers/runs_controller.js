b851085379d6637bc3bacfc67aa27a39e67051d6
alan <alwat91@uga.edu>
2017-03-09 12:08:41 -0500

    Runs now show on home page

--- public/scripts/controllers/runs_controller.js
+++ public/scripts/controllers/runs_controller.js
@@ -6,6 +6,13 @@ function RunsController($http, $state, $scope){
     $http.get(`${server}/users/${$scope.currentUser.id}/runs`)
       .then(function(res){
           self.allRuns = res.data.runs
+
+          self.allRuns.forEach(function(el){
+            el.date = new Date(el.beginning_time).toDateString();
+
+            el.beginning_time = new Date(el.beginning_time).toTimeString();
+            el.end_time = new Date(el.end_time).toTimeString();
+          })
       })
   }
   self.getRuns = getRuns;
